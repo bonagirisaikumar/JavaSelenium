a49b6ac906dd4b83e1418fb5d41117bb9fcee20e
bonagirisaikumar <61381496+bonagirisaikumar@users.noreply.github.com>
2020-02-23 23:42:27 +0530

    Basic Practice

---
+++ Main.java
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+    public static void main(String[] args)
+{
+    System.out.println("Hello Java First program");
+
+ String name = "abhi";
+ String name1 = "Surab"; 
+   int  I = 1;
+    int a= 5;
+    int b= 10;
+    System.out.println(I + a + "  is the final answer");
+    System.out.println(a > I);
+    int x=2, y=3, z=4;
+    int z1= x+y+z;
+    System.out.println(z1);
+ String alphabet= "abcdefghijklmnopqrstuvwxyz";
+ System.out.println("Total count" + " " + alphabet.length());
+ System.out.println(alphabet.toUpperCase());
+ System.out.println(alphabet.toLowerCase());
+ System.out.println(alphabet.indexOf("j"));
+{
+     int x = 50;
+int y = 10;
+if
+ (x > y)
+ {
+  System.out.println("Hello World");
+ }
+ }
+}
+}
+
+
+
