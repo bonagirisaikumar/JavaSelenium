55cc882c38d06d4a95f74efc5e91c64c4ab5b4c8
bonagirisaikumar <61381496+bonagirisaikumar@users.noreply.github.com>
2020-04-05 11:10:27 +0530

    Xpath manual configuration with Tagname replaced  with *
    Xpath manual configuration with Tagname replaced  with *

--- Testcase2Salesforce.java
+++ Testcase2Salesforce.java
@@ -13,14 +13,30 @@ public class Testcase2Salesforce {
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Salenium Repository\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-		driver.get("https://login.salesforce.com/?locale=in");
+		driver.get("https://login.salesforce.com/");
 		System.out.println(driver.getTitle());
 		System.out.println(driver.getCurrentUrl());
-		  
-		driver.findElement(By.id("username")).sendKeys("Saikumar.bonagiri");  	// Element finding using ID
+		// by class, ID and name   
+/*		driver.findElement(By.id("username")).sendKeys("Saikumar.bonagiri");  	// Element finding using ID
 	    driver.findElement(By.name("pw")).sendKeys("Password");					// Element finding using name
 		driver.findElement(By.className("button r4 wide primary")).click();		// Error -because compound classes are not allowed 
+*/
+		// by manual xpath configuration  syntax //tagname[@attribute='value']
+		
+/*		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("username");
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("Sa12345633");
+		driver.findElement(By.xpath("//input[@name='Login']")).click();
+		System.out.println(driver.findElement(By.xpath("//*[@id='error']")).getText());
 		driver.close();
+*/
+		// xPath Syntax modification to accept all inputs choosen  by replacing "Tagname" with '*'
+		
+		driver.findElement(By.xpath("//*[@id='username']")).sendKeys("username");
+		driver.findElement(By.xpath("//*[@type='password']")).sendKeys("Sa12345633");
+		driver.findElement(By.xpath("//*[@name='Login']")).click();
+		System.out.println(driver.findElement(By.xpath("//*[@id='error']")).getText());
+		
+		
 		//driver.quit();
 			
 	}
